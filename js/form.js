c1fd91e8bc948b84c666b0b75b25b836ddb2f395
miumio <nickolskaya777@yandex.ru>
2020-06-25 13:31:52 +0300

    some fixed 🚨

--- js/form.js
+++ js/form.js
@@ -4,6 +4,8 @@ var dataOn = form.querySelector("#data-coming");
 var dataOut = form.querySelector("#data-out");
 var adults = form.querySelector("#adults");
 
+form.classList.add('search-form-hide');
+
 formLink.addEventListener("click", function () {
   form.classList.toggle("search-form-hide");
   form.classList.remove("search-form-error");
